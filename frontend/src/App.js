2fd5703a807a133ebfd8ca8bc2f7ae611417ddc7
Eva <125091369+eva820@users.noreply.github.com>
2024-10-05 13:34:30 -0700

    initial landing page background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,18 @@ import Orrery from './Orrery.js';
 
 function App() {
   return (
-    <Orrery />
+    <div className="App">
+      <div className="header">
+        <p>
+          Capuchins | Orrery
+        </p>
+      </div>
+      <div className='container'>
+        <p>
+          ...
+        </p>
+      </div>
+    </div>
   );
 }
 
